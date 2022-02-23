38ef7178f095b5c7607c37ec8970396b2e7c8cd3
lankeycode <michael.moussaoui@gmail.com>
2022-02-23 23:35:09 +0100

    install headless ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,15 @@
+// import { ChakraProvider } from '@chakra-ui/react'
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <ChakraProvider>
+//       <Component {...pageProps} />
+//     </ChakraProvider>
+//   )
+// }
+
+// export default MyApp
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
