674e39bb93d93c9cd853ccff6f19ad31b99b38a4
lankeycode <michael.moussaoui@gmail.com>
2022-02-23 23:43:03 +0100

    add 4 pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,3 @@
-// import { ChakraProvider } from '@chakra-ui/react'
-
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <ChakraProvider>
-//       <Component {...pageProps} />
-//     </ChakraProvider>
-//   )
-// }
-
-// export default MyApp
-
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
