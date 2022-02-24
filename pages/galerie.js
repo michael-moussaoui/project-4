187f693712c7cb8d67e8f43e7f2801783fe1f5bc
lankeycode <michael.moussaoui@gmail.com>
2022-02-24 15:52:35 +0100

    install cloudinary

--- pages/galerie.js
+++ pages/galerie.js
@@ -1,5 +1,14 @@
 
 import Link from "next/link";
+import {CloudinaryImage} from "@cloudinary/url-gen/assets/CloudinaryImage";
+import {
+			 AdvancedImage,
+			 accessibility,
+			 responsive,
+			 lazyload,
+			 placeholder
+			} from '@cloudinary/react';
+
 
 export default function galerie() {
     
