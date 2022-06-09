0060d71fdd8c6c008eb69eab23ebdfc5b20dd493
lankeycode <michael.moussaoui@gmail.com>
2022-06-09 18:00:53 +0200

    display accessToken & space

--- pages/galerie.js
+++ pages/galerie.js
@@ -11,9 +11,10 @@ import Cursor from "../components/Cursor"
 
 const contentful = require('contentful')
 const client = contentful.createClient({
-  accessToken: process.env.CONTENTFUL_ACCES_TOKEN ,
-  space: process.env.CONTENTFUL_SPACE_ID,
-  
+  // accessToken: process.env.CONTENTFUL_ACCES_TOKEN ,
+  accessToken:"4T-4oJ_oey9Ow58J_AmzLOh1XD363P5ZGUqnkUBDPCk",
+  // space: process.env.CONTENTFUL_SPACE_ID,
+  space:"f0n48ont8v3m"
 });
 
 export default function Galerie({ galeries}  ) {
