8b51ddde8a52e67bc0ca8785e3287d46bd6dcb3f
lankeycode <michael.moussaoui@gmail.com>
2022-06-09 18:26:49 +0200

    create .env.production & .env.development

--- pages/galerie.js
+++ pages/galerie.js
@@ -11,10 +11,9 @@ import Cursor from "../components/Cursor"
 
 const contentful = require('contentful')
 const client = contentful.createClient({
-  // accessToken: process.env.CONTENTFUL_ACCES_TOKEN ,
-  accessToken:"4T-4oJ_oey9Ow58J_AmzLOh1XD363P5ZGUqnkUBDPCk",
-  // space: process.env.CONTENTFUL_SPACE_ID,
-  space:"f0n48ont8v3m"
+  accessToken: process.env.CONTENTFUL_ACCES_TOKEN ,
+  space: process.env.CONTENTFUL_SPACE_ID,
+  
 });
 
 export default function Galerie({ galeries}  ) {
--- pages/tarifs.js
+++ pages/tarifs.js
@@ -21,10 +21,9 @@ import styles from "../components/tarifs.module.css"
 
 const contentful = require('contentful')
 const client = contentful.createClient({
-  // accessToken: process.env.CONTENTFUL_ACCES_TOKEN ,
-  accessToken:"4T-4oJ_oey9Ow58J_AmzLOh1XD363P5ZGUqnkUBDPCk",
-  // space: process.env.CONTENTFUL_SPACE_ID,
-  space:"f0n48ont8v3m"
+  accessToken: process.env.CONTENTFUL_ACCES_TOKEN ,
+  space: process.env.CONTENTFUL_SPACE_ID,
+ 
   
 });
 
