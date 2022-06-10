cd23b6016662e2bfbe736321ba84f096f35e727e
lankeycode <michael.moussaoui@gmail.com>
2022-06-10 17:01:27 +0200

    gitignore modified

---
+++ utils/client.js
@@ -0,0 +1,10 @@
+import * as contentful from "contentful"
+
+
+export const client = contentful.createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCES_TOKEN ,
+    
+    
+  });
+  
