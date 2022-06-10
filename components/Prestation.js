c54ecfef5cf792b3f0527b1fd264ce229450e989
lankeycode <michael.moussaoui@gmail.com>
2022-06-10 09:26:43 +0200

    test for npm run build

--- components/Prestation.js
+++ components/Prestation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect, useCallback } from 'react';
-import { client } from '../utils/client'
+// import { client } from '../utils/client'
 import { motion } from 'framer-motion';
 import Cards from './Cards'
 import { Container,
