40563fb16bedd34c853cdcacf58e21d8c11d5bf2
lankeycode <michael.moussaoui@gmail.com>
2022-06-11 19:55:04 +0200

    test scroll btn

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -7,7 +7,7 @@ const ScrollToTop = () => {
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
-            if(window.scrollY > 400) {
+            if(window.scrollY > 50) {
                 setShowTopBtn(true)
             } else {
                 setShowTopBtn(false)
@@ -24,13 +24,14 @@ const ScrollToTop = () => {
     }
   return (
     <div className="top-to-btn">
-    {" "}
+    {/* {" "} */}
     {showTopBtn && (
     <FaArrowAltCircleUp 
          className="icon-position icon-style" 
          onClick={goToTop}
     />
-    )}{" "}
+    )}
+    {/* {" "} */}
     </div>
   )
 }
